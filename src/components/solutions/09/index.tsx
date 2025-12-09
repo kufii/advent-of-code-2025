5382f6a5f897553aaa276cff9f40341ef406897f
Adrien Pyke <adrien.pyke@tc.gc.ca>
2025-12-09 14:23:39 -0500

    optimize day 9

--- src/components/solutions/09/index.tsx
+++ src/components/solutions/09/index.tsx
@@ -1,4 +1,3 @@
-import { createEffect, createSignal, onCleanup, Show } from 'solid-js'
 import Answer from '../../Answer'
 import { desc, Point, sortBy } from '../util'
 import input from './input'
@@ -52,65 +51,43 @@ const orderPoints = (points: Point[]) => {
   return ordered
 }
 
-const createCache = (points: Point[]) => {
-  const perimeter = new Map<string, boolean>()
-  for (let i = 0; i < points.length; i++) {
-    const current = points[i]
-    const next = points[(i + 1) % points.length]
-
-    if (current.x === next.x) {
-      for (
-        let y = Math.min(current.y, next.y);
-        y <= Math.max(current.y, next.y);
-        y++
-      ) {
-        perimeter.set(key({ x: current.x, y }), true)
-      }
-    } else if (current.y === next.y) {
-      for (
-        let x = Math.min(current.x, next.x);
-        x <= Math.max(current.x, next.x);
-        x++
-      ) {
-        perimeter.set(key({ x, y: current.y }), true)
-      }
-    }
-  }
-  return perimeter
-}
-
-const isInsidePerimeter = (
-  point: Point,
-  perimeter: Point[],
-  cache: Map<string, boolean>,
-) => {
-  const k = key(point)
-  if (cache.has(k)) return cache.get(k)!
-
-  let inside = false
-
+const lineIntersectsPerimeter = (a: Point, b: Point, perimeter: Point[]) => {
   for (let i = 0; i < perimeter.length; i++) {
     const current = perimeter[i]
     const next = perimeter[(i + 1) % perimeter.length]
 
     if (
-      current.y > point.y !== next.y > point.y &&
-      point.x <
-        ((next.x - current.x) * (point.y - current.y)) / (next.y - current.y) +
-          current.x
+      (a.x === b.x && current.x === next.x) ||
+      (a.y === b.y && current.y === next.y)
+    )
+      continue
+
+    if (
+      a.x === b.x &&
+      Math.min(a.y, b.y) < current.y &&
+      current.y < Math.max(a.y, b.y) &&
+      a.x >= Math.min(current.x, next.x) &&
+      a.x <= Math.max(current.x, next.x)
     ) {
-      inside = !inside
+      return true
+    }
+
+    if (
+      a.y === b.y &&
+      Math.min(a.x, b.x) < current.x &&
+      current.x < Math.max(a.x, b.x) &&
+      a.y >= Math.min(current.y, next.y) &&
+      a.y <= Math.max(current.y, next.y)
+    ) {
+      return true
     }
   }
-
-  cache.set(k, inside)
-  return inside
+  return false
 }
 
 const rectIsInPerimeter = (
   { a, b }: { a: Point; b: Point },
   perimeter: Point[],
-  cache: Map<string, boolean>,
 ) => {
   const corners = [
     { x: Math.min(a.x, b.x), y: Math.min(a.y, b.y) },
@@ -118,32 +95,11 @@ const rectIsInPerimeter = (
     { x: Math.max(a.x, b.x), y: Math.max(a.y, b.y) },
     { x: Math.min(a.x, b.x), y: Math.max(a.y, b.y) },
   ]
-  if (!corners.every((corner) => isInsidePerimeter(corner, perimeter, cache)))
-    return false
 
   for (let i = 0; i < corners.length; i++) {
     const current = corners[i]
     const next = corners[(i + 1) % corners.length]
-
-    if (current.x === next.x) {
-      for (
-        let y = Math.min(current.y, next.y) + 1;
-        y <= Math.max(current.y, next.y) - 1;
-        y++
-      ) {
-        if (!isInsidePerimeter({ x: current.x, y }, perimeter, cache))
-          return false
-      }
-    } else if (current.y === next.y) {
-      for (
-        let x = Math.min(current.x, next.x) + 1;
-        x <= Math.max(current.x, next.x) - 1;
-        x++
-      ) {
-        if (!isInsidePerimeter({ x, y: current.y }, perimeter, cache))
-          return false
-      }
-    }
+    if (lineIntersectsPerimeter(current, next, perimeter)) return false
   }
   return true
 }
@@ -160,40 +116,15 @@ export function Part1() {
 }
 
 export function Part2() {
-  const [result, setResult] = createSignal<number | null>(null)
-  createEffect(() => {
-    setResult(null)
-    const perimeter = orderPoints(parseInput())
-    const rects = getAllRects(perimeter)
-    const cache = createCache(perimeter)
-    const loop = function* () {
-      let n = 0
-      for (const rect of rects) {
-        n++
-        if (n % 500 === 0) yield
-        if (rectIsInPerimeter({ a: rect.a, b: rect.b }, perimeter, cache)) {
-          setResult(rect.area)
-          return
-        }
-      }
-    }
-    const gen = loop()
-    const interval = setInterval(() => {
-      const { done } = gen.next()
-      if (done) clearInterval(interval)
-    }, 0)
-
-    onCleanup(() => clearInterval(interval))
-  })
+  const perimeter = orderPoints(parseInput())
+  const rects = getAllRects(perimeter)
+  const biggest = rects.find(({ a, b }) =>
+    rectIsInPerimeter({ a, b }, perimeter),
+  )!
   return (
     <p>
-      <Show
-        when={result() !== null}
-        fallback='Calculating... This takes a while...'
-      >
-        The largest area of any rectangle using only red and green tiles is{' '}
-        <Answer>{result()}</Answer>.
-      </Show>
+      The largest area of any rectangle using only red and green tiles is{' '}
+      <Answer>{biggest.area}</Answer>.
     </p>
   )
 }
